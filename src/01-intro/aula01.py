9b592213cb3c59d04f915de86f0be35c6bbd7100
joaogpizza <joao.piza@ufu.br>
2025-12-11 19:02:51 -0300

    commit inicial pt2

--- src/01-intro/aula01.py
+++ src/01-intro/aula01.py
@@ -1,1 +1,12 @@
-print('a')
+""" aula01 """
+
+print('olá mundo!!!')
+
+
+def somar(numero_01, numero_02):
+    """ somar - soma dois numeros """
+    return numero_01 + numero_02
+
+
+SOMA = somar(10.3, 2.5)
+print(SOMA)
