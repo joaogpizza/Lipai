c0365f28e69e8676f0e1e143e048780173009637
joaogpizza <joao.piza@ufu.br>
2025-12-15 13:33:49 -0300

    aulas S3.A1

--- src/05-debug/aulas.py
+++ src/05-debug/aulas.py
@@ -1,9 +1,9 @@
 """ Aula 01 - Debug """
 
+
 def somar(n1, n2, n3):
     soma = n1 + n2 + n3
     return soma
-
 
 
 def calcular_media(n1, n2, n3):
@@ -11,11 +11,13 @@ def calcular_media(n1, n2, n3):
     media = soma / 3
     return media
 
+
 nota1 = 10.0
 nota2 = 3.0
 nota3 = 5.5
 
 
 media = calcular_media(nota1, nota2, nota3)
+
 
 print(media)
